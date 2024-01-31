675776e93f596efb4e7398c2edc7b5654aebd73e
GadekarSid <gadekarsiddharth17@gmail.com>
2024-01-31 16:33:07 +0530

    Added entity and tested connection to db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,35 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.example.entities.Product;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EntityManager em;
+       // EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+            // Represents the context
+        EntityManagerFactory emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(),
+                new HashMap());
+        em = emf.createEntityManager();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try {
+            em.getTransaction().begin();
+            Product p = new Product();
+            p.setId(2L);
+            p.setName("Wine");
+            em.persist(p); // Add to context. -> Not an insert query.
+            em.getTransaction().commit();
+        }
+        finally {
+            em.close();
         }
     }
 }
