b298a64d2e6b5e702b7525e1377ab09874156e6c
GadekarSid <gadekarsiddharth17@gmail.com>
2024-02-05 15:05:19 +0530

    Added generated value

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,13 @@ package org.example;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.example.entities.Employee;
 import org.example.entities.Product;
 import org.example.persistence.CustomPersistenceUnitInfo;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import java.util.HashMap;
+import java.util.Map;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -16,16 +18,19 @@ public class Main {
         EntityManager em;
        // EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
             // Represents the context
+        Map<String,String> props = new HashMap<>();
+        props.put("hibernate.show_sql","true");
+        props.put("hibernate.hbm2ddl.auto","update");
         EntityManagerFactory emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(),
-                new HashMap());
+                props);
         em = emf.createEntityManager();
 
         try {
             em.getTransaction().begin();
-            Product p = new Product();
-            p.setId(2L);
-            p.setName("Wine");
-            em.persist(p); // Add to context. -> Not an insert query.
+            Employee e1 = em.getReference(Employee.class,1);
+            System.out.println(e1);
+            e1.setName("Daniel");
+            em.refresh(e1);
             em.getTransaction().commit();
         }
         finally {
