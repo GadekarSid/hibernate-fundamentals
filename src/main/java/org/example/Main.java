b6e2775fe2c2ea6592fefcf53ada58e5eb275858
GadekarSid <gadekarsiddharth17@gmail.com>
2024-02-06 15:08:55 +0530

    Composite keys added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.example.entities.Employee;
 import org.example.entities.Product;
+import org.example.entities.Student;
+import org.example.entities.keys.ProductKey;
+import org.example.entities.keys.StudentKey;
 import org.example.persistence.CustomPersistenceUnitInfo;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 
@@ -27,10 +30,20 @@ public class Main {
 
         try {
             em.getTransaction().begin();
-            Employee e1 = em.getReference(Employee.class,1);
-            System.out.println(e1);
-            e1.setName("Daniel");
-            em.refresh(e1);
+//            Product p1 = new Product();
+//            p1.setCode("abc");
+//            p1.setNumber(10);
+//            p1.setColor("Red");
+//            em.persist(p1);
+            StudentKey sk = new StudentKey();
+            sk.setCode("11241");
+            sk.setNumber(1151);
+//            Student student = new Student();
+//            student.setId(sk);
+//            student.setName("Siddhu");
+//            em.persist(student);
+            Student s = em.find(Student.class,sk);
+            System.out.println(s);
             em.getTransaction().commit();
         }
         finally {
